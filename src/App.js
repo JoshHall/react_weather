5f37ee78029e9d080a08cc38255ff2f6cbc3e840
Josh Hall <joshhall90@yahoo.com>
2019-03-06 11:39:23 -0500

    Rendering routes and entire wather component, TODO: setup form and api call

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header';
+import Weather from './views/weather';
+import Sample from './views/sample';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' render={() => <Weather />} />
+            <Route exact path='/sample' render={() => <Sample />} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'));
+
+
+
+
+// good for development, keeps page from refreshing, which means that as we develop the code it won't reset the state
+if (module.hot) {
+  module.hot.accept();
+}
